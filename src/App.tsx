83bf416ab48922dd9bbda8e50dd41db8d5c9cffd
Haltarys <gregoire.haardt@gmail.com>
2022-03-10 12:45:23 -0800

    feat: add basic toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import CodeIcon from '@mui/icons-material/Code';
+import FeedbackIcon from '@mui/icons-material/Feedback';
 
 function App() {
-  return <Button variant="contained">Click me!</Button>;
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          HIST 130 Quiz
+        </Typography>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Made with love ❤️ by Haltarys!
+        </Typography>
+        <Tooltip title="Send Feedback">
+          <IconButton size="large" color="inherit">
+            <FeedbackIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="View the source code on Github!">
+          <IconButton
+            size="large"
+            color="inherit"
+            href="https://github.com/Haltarys/HIST130-Quiz"
+            target="_blank"
+          >
+            <CodeIcon />
+          </IconButton>
+        </Tooltip>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default App;
