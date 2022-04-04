a9bf25ef9865d352fc6ea93849d3482245dc1b69
Haltarys <gregoire.haardt@gmail.com>
2022-04-03 19:53:37 -0700

    feat: add chapter title to favourited cards

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,4 +1,4 @@
-import { useDefinitions } from 'src/api';
+import { useChapters, useDefinitions } from 'src/api';
 import { useStore } from 'src/store';
 import Error from 'src/components/error';
 import DefinitionCard, { Collection } from 'src/components/card';
@@ -7,31 +7,44 @@ import { NoFavourites } from 'src/components/card';
 
 function Favourites() {
   const { isLoading, error, data: definitions } = useDefinitions();
+  const {
+    isLoading: areChaptersLoading,
+    error: chapterError,
+    data: chapters,
+  } = useChapters();
   const { favouriteIDs, onBookmark } = useStore();
 
   const favourites = definitions?.filter((definition) =>
     favouriteIDs.includes(definition.id),
   );
+  const areThereFavouritesSaved = favourites && favourites.length > 0;
 
   if (error) return <Error>{error?.message}</Error>;
+  if (chapterError) return <Error>{chapterError?.message}</Error>;
 
   return (
-    <Collection header="Favourites">
-      {isLoading ? (
+    <Collection header={areThereFavouritesSaved && 'Favourites'}>
+      {isLoading || areChaptersLoading ? (
         <LoadingCardList />
-      ) : favourites && favourites.length > 0 ? (
-        favourites.map((definition) => (
-          <DefinitionCard
-            key={definition.id}
-            definition={definition}
-            subheader={`${
-              // TODO: get chapter title somehow
-              'chapter.title'
-            } (page ${definition.pageNumber})`}
-            isFavourite={favouriteIDs.includes(definition.id)}
-            onBookmark={onBookmark}
-          />
-        ))
+      ) : areThereFavouritesSaved ? (
+        favourites.map((definition) => {
+          const chapterTitle = chapters?.find(
+            (chapter) => chapter.id === definition.chapterId,
+          )?.title;
+
+          return (
+            <DefinitionCard
+              key={definition.id}
+              definition={definition}
+              subheader={`${
+                // TODO: get chapter title somehow
+                chapterTitle
+              } (page ${definition.pageNumber})`}
+              isFavourite={favouriteIDs.includes(definition.id)}
+              onBookmark={onBookmark}
+            />
+          );
+        })
       ) : (
         <NoFavourites />
       )}
