aa4a2fd77fd91bdce14d4ca0c1810beadaf3fcc2
Haltarys <gregoire.haardt@gmail.com>
2022-04-15 19:45:54 -0700

    feat(store): persist store in local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import throttle from 'lodash.throttle';
 import favouritesSlice from 'src/features/favouritesSlice';
 import selectedChaptersSlice from 'src/features/selectedChaptersSlice';
+import { loadPersistedState, persistState } from './persist';
 
 const store = configureStore({
   reducer: {
     favourites: favouritesSlice,
     selectedChapters: selectedChaptersSlice,
   },
+  preloadedState: loadPersistedState(),
 });
+
+store.subscribe(
+  throttle(() => {
+    persistState(store.getState());
+  }, 2000),
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
