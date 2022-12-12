6a9f949113bf9571523a62c9c4e2b95b01325443
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:24:26 +0100

    feat(feedback): add notification when email is sent

---
+++ src/components/feedback/FeedbackAlert.tsx
@@ -0,0 +1,47 @@
+import Alert from '@mui/material/Alert';
+import Slide, { SlideProps } from '@mui/material/Slide';
+import Snackbar from '@mui/material/Snackbar';
+import type { FeedbackEmailResponse } from 'src/api';
+
+function SlideTransition(props: SlideProps) {
+  return <Slide {...props} direction="left" />;
+}
+
+interface FeedbackAlertProps {
+  feedback: FeedbackEmailResponse;
+  clearFeedback: () => void;
+}
+
+function FeedbackAlert({
+  feedback: { success, message },
+  clearFeedback,
+}: FeedbackAlertProps) {
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason !== 'clickaway') clearFeedback();
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      open
+      message={message}
+      autoHideDuration={6000}
+      TransitionComponent={SlideTransition}
+      onClose={handleClose}
+    >
+      <Alert
+        variant="filled"
+        severity={success ? 'success' : 'error'}
+        sx={{ width: '100%' }}
+        onClose={handleClose}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
+
+export default FeedbackAlert;
