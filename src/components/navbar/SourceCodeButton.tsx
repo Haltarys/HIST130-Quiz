c9255f275ee24b092d48235757b4746e69767175
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:24:26 +0100

    fix: add noreferrer noopener security measures to external redirection

--- src/components/navbar/SourceCodeButton.tsx
+++ src/components/navbar/SourceCodeButton.tsx
@@ -10,6 +10,7 @@ function SourceCodeButton() {
         color="inherit"
         href="https://github.com/Haltarys/HIST130-Quiz"
         target="_blank"
+        rel="norefferer noopener"
       >
         <CodeIcon />
       </IconButton>
