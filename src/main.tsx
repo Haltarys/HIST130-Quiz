5f9ae4631d5b2dab30bf115c18a6fa5b7426dc99
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:24:26 +0100

    feat: add custom MUI theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </CssBaseline>
   </React.StrictMode>,
   document.getElementById('root'),
