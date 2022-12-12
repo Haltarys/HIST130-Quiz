03370bfa4b99fc8f4f7e4bccedac8c531733d103
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:24:26 +0100

    fix: move center logic to error component from 'not found' page

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,5 +1,6 @@
-import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 
 interface ErrorProps {
   children: React.ReactNode;
@@ -7,19 +8,21 @@ interface ErrorProps {
 
 function Error({ children }: ErrorProps) {
   return (
-    // Using Typography instead of Box because the color doesn't propagate otherwise.
-    <Typography
-      color="error"
-      // Rendered as a div because React complains that headers (<h6>) must not be rendered inside a <p> tag (default value.)
-      component="div"
-      textAlign="center"
-    >
-      <SentimentVeryDissatisfiedIcon color="error" sx={{ fontSize: 96 }} />
-      <Typography variant="h4" color="error">
-        An error occurred:
+    <Box height="100%" display="grid" sx={{ placeItems: 'center' }}>
+      {/* Using Typography instead of Box because the color doesn't propagate otherwise. */}
+      <Typography
+        color="error"
+        // Rendered as a div because React complains that headers (<h6>) must not be rendered inside a <p> tag (default value.)
+        component="div"
+        textAlign="center"
+      >
+        <SentimentVeryDissatisfiedIcon color="error" sx={{ fontSize: 96 }} />
+        <Typography variant="h4" color="error">
+          An error occurred:
+        </Typography>
+        {children || 'Unknown error.'}
       </Typography>
-      {children || 'Unknown error.'}
-    </Typography>
+    </Box>
   );
 }
 
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,3 @@
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import { Link as RouterLink } from 'react-router-dom';
@@ -6,29 +5,21 @@ import Error from 'src/components/error';
 
 function NotFound() {
   return (
-    <Box
-      height="100%"
-      display="grid"
-      sx={{
-        placeItems: 'center',
-      }}
-    >
-      <Error>
-        <Typography variant="h6">Page not found.</Typography>
-        <Typography variant="body1">
-          Go to{' '}
-          <Link
-            color="error"
-            to="/"
-            component={RouterLink}
-            sx={{ fontWeight: 'bold' }}
-          >
-            homepage
-          </Link>
-          .
-        </Typography>
-      </Error>
-    </Box>
+    <Error>
+      <Typography variant="h6">Page not found.</Typography>
+      <Typography variant="body1">
+        Go to{' '}
+        <Link
+          color="error"
+          to="/"
+          component={RouterLink}
+          sx={{ fontWeight: 'bold' }}
+        >
+          homepage
+        </Link>
+        .
+      </Typography>
+    </Error>
   );
 }
 
