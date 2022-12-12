c550a2aff7f22a8eb2061f825890df38a4d759b0
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 23:24:26 +0100

    fix: center only error messages (and not favourites)

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,25 +1,34 @@
-import Error from 'src/components/error';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import { Link as RouterLink } from 'react-router-dom';
+import Error from 'src/components/error';
 
 function NotFound() {
   return (
-    <Error>
-      <Typography variant="h6">Page not found.</Typography>
-      <Typography variant="body1">
-        Go to{' '}
-        <Link
-          color="error"
-          to="/"
-          component={RouterLink}
-          sx={{ fontWeight: 'bold' }}
-        >
-          homepage
-        </Link>
-        .
-      </Typography>
-    </Error>
+    <Box
+      height="100%"
+      display="grid"
+      sx={{
+        placeItems: 'center',
+      }}
+    >
+      <Error>
+        <Typography variant="h6">Page not found.</Typography>
+        <Typography variant="body1">
+          Go to{' '}
+          <Link
+            color="error"
+            to="/"
+            component={RouterLink}
+            sx={{ fontWeight: 'bold' }}
+          >
+            homepage
+          </Link>
+          .
+        </Typography>
+      </Error>
+    </Box>
   );
 }
 
